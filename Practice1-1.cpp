5c2035b5d65593e4d9d7da3ea5143065d7950e67
Taxod <s885537@gmail.com>
2018-03-05 12:58:48 +0800

    2

--- Practice1-1.cpp
+++ Practice1-1.cpp
@@ -5,46 +5,51 @@ using namespace std;
 class MyVector
 {
 private:
-int n;
-double* m;
+	int n;
+	double* m;
 public:
-MyVector();
-MyVector(int n, double m[]);
-MyVector(const MyVector& v);
-~MyVector();
-void print();
+	MyVector();
+	MyVector(int n, double m[]);
+	MyVector(const MyVector& v);
+	~MyVector();
+	void print();
 };
 
 int main(){
 	return 0;
 }
+
 MyVector::MyVector()
 {
-n = 0;
-m = nullptr;
+	n = 0;
+	m = nullptr;
 }
+
 MyVector::MyVector(int dim, double v[])
 {
-n = dim;
-m = new double[dim];
-for(int i = 0; i < dim; i++)
-m[i] = v[i];
+	n = dim;
+	m = new double[dim];
+	for(int i = 0; i < dim; i++)
+	m[i] = v[i];
 }
+
 MyVector::~MyVector()
 {
-delete [] m;
+	delete [] m;
 }
+
 MyVector::MyVector(const MyVector& v)
 {
-n = v.n;
-m = new double[n];
-for(int i = 0; i < n; i++)
-m[i] = v.m[i];
+	n = v.n;
+	m = new double[n];
+	for(int i = 0; i < n; i++)
+		m[i] = v.m[i];
 }
+
 void MyVector::print()
 {
-cout << "(";
-for(int i = 0; i < n - 1; i++)
-cout << m[i] << ", ";
-cout << m[n-1] << ")\n";
+	cout << "(";
+	for(int i = 0; i < n - 1; i++)
+		cout << m[i] << ", ";
+	cout << m[n-1] << ")\n";
 }
