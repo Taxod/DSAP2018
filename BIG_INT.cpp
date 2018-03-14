b3de44485ecda39dc306c6d40d7a8134b9bc47e4
Taxod <s885537@gmail.com>
2018-03-14 16:07:51 +0800

    -

--- BIG_INT.cpp
+++ BIG_INT.cpp
@@ -15,32 +15,36 @@ class big_int{
 		big_int();
 		big_int(char *c);
 		big_int(string& c);
-		~big_int();
+		~big_int();	
+		big_int operator+(big_int q);
 		friend main();
 };
 int main(){
 	string k = "1111111111";
 	big_int p(k);
 	big_int q(k);
-	
-	
-	for(int i = 0 ; i < p.len ; i++){
-		cout << p.cal[i].n<<" ";
+	big_int t = p.operator+(q);
+	cout << t.len;
+	for(int i = 0 ; i < t.len ; i++){
+		cout << t.cal[i].n<<" ";
 	}
-	cout << endl;
-	for(int i = 0 ; i < p.len ; i++){
-		cout << p.cal[i].p<<" ";
-	}
+//	for(int i = 0 ; i < p.len ; i++){
+//		cout << p.cal[i].n<<" ";
+//	}
+//	cout << endl;
+//	for(int i = 0 ; i < p.len ; i++){
+//		cout << p.cal[i].p<<" ";
+//	}
 	return 0;
 }
-big_int operator+(big_int p,big_int q){
+big_int big_int::operator+(big_int q){
 	big_int result;
-	int biglen = p.len;
-	if(q.len > p.len){
+	int biglen = this->len;
+	if(q.len > this->len){
 		biglen = q.len;
 	}
 	for(int i = 0 ; i < biglen ; i++){
-		
+		result.cal[i].n = q.cal[i].n + this->cal[i].n;
 	}
 	return result;
 } 
