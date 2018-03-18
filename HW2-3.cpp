ffbc1a115236da83ebad84971771eba1ca0db992
Taxod <s885537@gmail.com>
2018-03-18 18:13:58 +0800

    -

--- HW2-3.cpp
+++ HW2-3.cpp
@@ -2,14 +2,15 @@
 #include<string>
 
 using namespace std;
-size_t getSpaceCount(const size_t from, const size_t to,string T);
+size_t getSpaceCount(const size_t from, const size_t to,string K);
 int main(){
 	string T = "123 456 879";
 	cout << getSpaceCount(0,T.length(),T)<< endl;
 	cout << T;	
 	return 0;
 }
-size_t getSpaceCount(const size_t from, const size_t to,string T){
+size_t getSpaceCount(const size_t from, const size_t to,string K){
+	string T = K;
 	size_t found = T.find_first_of(' ');
 	int count = 0;
 	while(found != string::npos){
