42d63f168e9c548d032393d1d7ebdcb3fe94457b
Taxod <s885537@gmail.com>
2018-03-30 00:12:26 +0800

    -

--- HWP1_Bonus.cpp
+++ HWP1_Bonus.cpp
@@ -1,14 +1,15 @@
 #include <vector>
 #include <iostream>
-#include <String>
+#include <string>
+#include <math.h>
+#include <cmath>
 using namespace std;
 
+static int vcnt = 0;
 class Myvector
 {
 private:
-	vector<int> v;
-	string id;
-	int real_len;
+
 public:
 	Myvector();
 	void operator=(Myvector q);
@@ -16,28 +17,64 @@ public:
 	// Myvector operator-();
 	// void operator[](int n,int value);
 	// Myvector operator<();
-	// Myvector operator>();
+	bool operator>(Myvector q);
 	bool operator==(Myvector q);
+	// void print();
 	~ Myvector();
-	friend void declare(Myvector ** ptr,int vcnt);
+	//------------------
+	vector<int> v;
+	string id;
+	int real_len;
+	//------------------
+	friend void declare(Myvector ** ptr);
+	friend int main();
 };
-void declare(Myvector **ptr,int vcnt);
+void declare(Myvector **ptr);
+void assign(Myvector **ptr,string name);
+bool compare(Myvector **ptr);
 
+/*
+N a 3 0 0 0
+N b 3 0 0 0
+C E a b 
+A a 8 3 4 3 4 3 4 5 6
+C E a b
+C G a b
+C L a b
+N c 2 -1 -2
+C L c b
+C L a c
 
-int main(int argc, char const *argv[])
+S 4 a a b c
+I a 1 0
+
+*/
+//1
+//0
+//1
+//1
+//0
+//0
+
+int main()
 {
+
 	Myvector** ptr;
 	ptr = new Myvector* [100];
-	int vcnt = 0;
 	char c;
+
+	string _name;
 	while(cin >> c){
 		switch(c){
 			case 'N'://declare
-				declare(ptr,vcnt);
+				declare(ptr);
 				break;
 			case 'A'://assignment
+				cin >> _name;
+				assign(ptr,_name);
 				break;
 			case 'C'://comparison
+				cout << compare(ptr)<<"\n";
 				break;
 			case 'I'://indexing
 				break;
@@ -46,21 +83,132 @@ int main(int argc, char const *argv[])
 			case 'M'://minus
 				break;
 		}
+		// ptr[vcnt]-> print();
+		// cout << "-------\n";
 	}
 	
 	return 0;
 }
+bool Myvector::operator>(Myvector q){
+	int gap;
+	gap = this->real_len - q.real_len;
+	gap = abs(gap);
+	if (this->real_len > q.real_len)
+	{
+		for (int i = 0; i < gap; ++i)
+		{
+			q.v.push_back(q.v[i%real_len]);
+		}
+
+	// cout << "--------\n";
+	// this->print();
+	// for (int i = 0; i < this->real_len; ++i)
+	// {
+	// 	cout << q.v[i] <<" ";
+	// }
+	// cout << endl;
+	// cout << "----------\n";
+		for (int i = 0; i < this->real_len; ++i)
+		{
+			if (this->v[i] < q.v[i])
+			{
+				return false;
+			}
+		}
+	}else if (this->real_len < q.real_len)
+	{
+		for (int i = 0; i < gap; ++i)
+		{
+			this->v.push_back(this->v[i % real_len]);
+		}
+	// cout << "--------\n";
+	// for (int i = 0; i < q.real_len; ++i)
+	// {
+	// 	cout << this->v[i] << " ";
+	// }
+	// cout <<endl; 
+	// q.print();
+	// cout << "----------\n";
+		for (int i = 0; i < q.real_len; ++i)
+		{
+			if (this->v[i] < q.v[i])
+			{
+				return false;
+			}
+		}
+	}
+	for (int i = 0; i < q.real_len; ++i)
+	{
+		if (this->v[i] < q.v[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+bool compare(Myvector **ptr){
+	char op;
+	cin >> op;
+	string A,B;
+	cin >> A;
+	cin >> B;
+	Myvector sub ,d;
+	for (int i = 0; i < vcnt; ++i)
+	{
+		if (ptr[i]->id == A)
+		{
+			sub = *ptr[i];
+		}else if (ptr[i]->id == B)
+		{
+			d = *ptr[i];
+		}
+	}
+	switch(op){
+		case 'L':
+			if (sub > d || sub == d){
+				return false;
+			}else{
+				return true;
+			}
+		case 'G':
+			return sub > d;
+		case 'E':
+			return sub == d;
+	}
+	return 0;
+}
+void assign(Myvector **ptr,string name){
+	int numcnt;
+	cin >> numcnt;
+	for (int i = 0; i < vcnt; ++i)
+	{
+		if (ptr[i]->id == name)
+		{
+			ptr[i]->v.clear();
+			ptr[i]->real_len = numcnt;
+			for (int j = 0; j < numcnt; ++j)
+			{
+				int tmp;
+				cin >> tmp;
+				ptr[i]->v.push_back(tmp);
+			}
+			// ptr[i]->print();
+			break;
+		}
+	}
+
+}
 
 
-
-void declare(Myvector **ptr,int vcnt){
+void declare(Myvector **ptr){
 	string name;
+	int count;
+	// cout << "declare\n";
 	cin >> name;
+	cin >> count;
 	ptr[vcnt] = new Myvector();
 	ptr[vcnt]->id = name;
-	int count;
 	ptr[vcnt]->real_len = count;
-	cin >> count;
 	for (int i = 0; i < count; ++i)
 	{
 		int tmp;
@@ -80,7 +228,7 @@ Myvector::~Myvector(){
 void Myvector::operator=(Myvector q){
 	this->v = q.v;
 	this->real_len = q.real_len;
-	// this->id = q.id;--------------------
+	this->id = q.id;//--------------------
 }
 
 bool Myvector::operator==(Myvector q){
@@ -97,4 +245,13 @@ bool Myvector::operator==(Myvector q){
 }
 // void Myvector::operator[](int n,int value){
 // 	this->v[n] = value;
+// }
+// void Myvector::print(){
+// 	cout << "id: " << id << endl;
+// 	cout << "len:" << real_len << endl; 
+// 	for (int i = 0; i < real_len; ++i)
+// 	{
+// 		cout << v[i] << " ";
+// 	}
+// 	cout << endl;
 // }
