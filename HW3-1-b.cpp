8ed5e42cd72197ee9345ba891b30c2326b789ba8
Taxod <s885537@gmail.com>
2018-04-06 18:58:45 +0800

    -

--- HW3-1-b.cpp
+++ HW3-1-b.cpp
@@ -2,26 +2,19 @@
 using namespace std;
 
 class Client{
+	int Stock;
+	int Bond; 
+	int Fund; 
+	int Insurance;
 };
 class Platinum_Client : public Client{
-	int Stock;
-	int Bond; 
-	int Fund; 
-	int Insurance;
+	
 };
 class High_Level_Client : public Client{
-	int Stock;
-	int Bond; 
-	int Fund; 
-	int Insurance;
 };
 class Normal_Client : public Client{
-	int Stock;
-	int Bond; 
-	int Fund; 
-	int Insurance;
 };
-int main(){
-	Client* bank[100];	
+int main(){	
+	Client* bank[1000];
 	return 0;
 } 
