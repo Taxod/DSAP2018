bccbd4e50d3905aa487da54c3e0b0494dd3a807b
Taxod <s885537@gmail.com>
2018-04-18 17:03:20 +0800

    -

--- Ride_sharing.cpp
+++ Ride_sharing.cpp
@@ -1,14 +1,57 @@
 #include <iostream>
 #include <string>
-// #include <stdio.h>
-
+#include<stdlib.h>
 using namespace std;
+template<typename ItemType>
+class Node
+{
+private:
+  ItemType item;
+  Node<ItemType>* next; 
+public:
+  Node();
+  Node(const ItemType& anItem);
+  Node(const ItemType& anItem, Node<ItemType>* nextNodePtr);
+  void setItem(const ItemType& anItem);
+  void setNext(Node<ItemType>* nextNodePtr);
+  ItemType getItem() const ;
+  Node<ItemType>* getNext() const ;
+}; 
+template<typename ItemType>
+class Bag
+{
+private:
+  Node<ItemType>* headPtr;
+  int itemCount;
+  Node<ItemType>* getPointerTo(const ItemType& target) const; 
+public:
+  Bag();
+  Bag(const Bag<ItemType>& aBag); // Copy constructor
+  ~Bag();                       
+  int getCurrentSize() const;
+  bool isEmpty() const;
+  bool add(const ItemType& newEntry);
+  bool remove(const ItemType& anEntry);
+  void clear();
+  bool contains(const ItemType& anEntry) const;
+  Node<ItemType>* get(const ItemType& target);
+  Node<ItemType>* get(string target);
+  Node<ItemType>* getfirstnode(){
+  	return headPtr;
+  }
+  
+  // int getFrequencyOf(const ItemType& anEntry) const;
+  // vector<ItemType> toVector() const;
+};
+
+
+
+//using namespace std;
 struct loc{
 	int x;
 	int y;
 };
 
-//讀的時候把全部的標籤記下來，定訂次序，讀資料的時候比對、設定對應bool
 class Entity{
 protected:
 	string id;//ID
@@ -18,9 +61,39 @@ protected:
 	bool* attribute;//標籤 bool array
 	int time;//上一次更新時間 int
 public:
-	Entity(string id,bool ison,bool isser,loc location,bool* attribute,int time);
+	Entity(string id,bool* attribute,int time);
 	Entity();
 	virtual void print();
+	string getid();
+	loc getlocation();
+	void setlocation(loc c){
+		location = c;
+	}
+	void settime(int time){
+		this->time = time;
+	}
+	void setison(bool on){
+		ison = on;
+	}
+	int gettime(){
+		return time;
+	}
+};
+loc Entity::getlocation(){
+	return location;
+}
+string Entity::getid(){
+	return id;
+}
+class Passenger : public Entity
+{
+private:
+public:
+	Passenger();
+	Passenger(string id,bool *attribute,int time);
+	void print();
+	// ~passenger();
+	
 };
 
 class car : public Entity{
@@ -32,96 +105,222 @@ protected:
 	//基本價、單位里程價
 public:
 	car();
-	car(string id,bool ison,bool isser,loc location,bool*attribute,int time,int score,bool high_level,char direction,int judge_time);
+	car(string id,bool*attribute,int time,bool high_level);
 	void print();
+	char getdirection();
+	void setdirection(char c);
+	bool getlevel(){
+		return high_level;
+	}
 };
+void car::setdirection(char c){
+	direction = c;
+}
+char car::getdirection(){
+	return direction;
+}
+int chargetime(string T){
+	// int time = 0;
+	string tmp = T.substr(0,T.find(':'));
+	T = T.substr(T.find(':')+1,string::npos);
+	int hour_ten = tmp[0]-'0';
+	int hour_one = tmp[1]-'0';
+	int hour = hour_ten*10 + hour_one;
+	int minute_ten = T[0] - '0';
+	int minute_one = T[1] - '0';
+	int minute = minute_ten*10 + minute_one;
+	int time = hour*60 + minute;
+	return time;
+}
+
+car NC_condition(string s,int time,string total_attribute[],int attributeN);
+Passenger NP_condition(string s,int time,string total_attribute[],int attributeN);
+// template<typename ItemType>
+void OC_condition(string s,int time,Bag<car> car_bag){
+	/*cout << "*\n";
+	
+*/
+}
 
 
 
-int main(int argc, char const *argv[])
+
+int main()
 {
-	// int max_dis,ar,br,al,bl,k,h,p,attributeN;
-	// cin >> max_dis >> ar >> br >> al >> bl >> k >> h >> p >> attributeN;
-	// string trash;
-	// getline(cin,trash);
-	// string total_atrribute[];
-	// getline(cin,tmp_a);
+	int max_dis,ar,br,al,bl,k,h,p,attributeN;
+	cin >> max_dis >> ar >> br >> al >> bl >> k >> h >> p >> attributeN;
+	string trash;
+	getline(cin,trash);
+	string tmp_a;
+	string* total_attribute = new string [attributeN];
+	getline(cin,tmp_a);
+	
+	for (int i = 0; i < attributeN-1; ++i)
+	{
+		string atmp = tmp_a.substr(0,tmp_a.find(';'));
+    	tmp_a.erase(0,tmp_a.find(';')+1);
+    	total_attribute[i] = atmp;	
+	}
+	total_attribute[attributeN-1] = tmp_a;
+
 	string s;
 	/*
-	00:01 NP:0987654321(love,enjoy,hate)
+	00:01 NC:AAA111(love,enjoy,hate)L
+	00:05 NP:0987654321(love,enjoy,hate)
+	00:12 OC:AAA111(1,5)N
+	00:19 EC:AAA111(E)
 	*/
+	Bag<car> car_bag;
+	Bag<Passenger> Passenger_bag;
 	while(getline(cin,s)){
-		int time = 0;
-		if (cin.eof())
-		{
-			break;
-		}
 		string T = 	s.substr(0,s.find(' ')); // T 時間字串 ex : 00:00
+		int time = chargetime(T);
 		s = s.substr(s.find(' ')+1,string::npos);
 		string condition = s.substr(0,s.find(':'));
 		s = s.substr(s.find(':')+1,string::npos);
 		if (condition == "NP")
-		{
-			/* code */
+		{	
+			Passenger_bag.add(NP_condition(s,time,total_attribute,attributeN));
 		}else if (condition == "NC")
 		{
-			/* code */
+			car_bag.add(NC_condition(s,time,total_attribute,attributeN));
 		}else if (condition == "OC")
 		{
-			/* code */
+			string target = s.substr(0,s.find('('));
+			Node<car>* car_node_ptr = car_bag.get(target);
+			loc p;
+			p.x = stoi(s.substr(s.find('(')+1,s.find(',')));
+			p.y = stoi(s.substr(s.find(',')+1,s.find(')')));
+			char c = s.substr(s.find(')')+1,string::npos)[0];
+			if (car_node_ptr != nullptr)
+			{
+				car tt = car_node_ptr->getItem();
+				tt.setdirection(c);
+				tt.setlocation(p);
+				tt.settime(time);
+				tt.setison(true);
+				car_node_ptr->setItem(tt);
+			}
+			car_node_ptr->getItem().print();
 		}else if (condition == "EC")
 		{
-			/* code */
+			string target = s.substr(0,s.find('('));
+			Node<car>* car_node_ptr = car_bag.get(target);
+			char c = s.substr(s.find('(')+1,s.find(')'))[0];
+			if (car_node_ptr != nullptr)
+			{
+				car tt = car_node_ptr->getItem();
+				int timegap = time - tt.gettime();
+				char d = tt.getdirection();
+				loc tmp = tt.getlocation();
+				switch(d){
+					case 'N':
+						tmp.y += timegap;
+						break;
+					case 'S':
+						tmp.y -= timegap;
+						break;
+					case 'W':
+						tmp.x -= timegap;
+						break;
+					case 'E':
+						tmp.x += timegap;
+						break;
+					case 'H':
+						break;
+				}
+				tt.setlocation(tmp);
+				tt.setdirection(c);
+				tt.settime(time);
+				car_node_ptr->setItem(tt);
+			}
+			car_node_ptr->getItem().print();
 		}else if (condition == "OP")
 		{
-			/* code */
+			//0987654321(6,10)L
+			string target = s.substr(0,s.find('('));
+			Node<Passenger>* Passenger_node_ptr = Passenger_bag.get(target);
+			string suit_id;
+			char le = s.substr(s.find(')')+1,string::npos)[0];
+			bool need_car_level = 0;
+			if (le != 'R')
+			{
+				need_car_level = 1;
+			}
+			int max_suit;
+			Node<car>* first = car_bag.getfirstnode();
+			if (Passenger_node_ptr != nullptr)
+			{
+				for (int i = 0; i < car_bag.getCurrentSize(); ++i)
+				{
+					if (first->getItem().getlevel() == need_car_level)
+					{
+						//距離篩選
+						//適配度計算
+					}
+				}
+			}
+			
 		}else if (condition == "CP")
 		{
-			/* code */
+			
 		}else if (condition == "AD")
 		{
-			/* code */
+			
 		}else if (condition == "LC")
 		{
-			/* code */
+			
 		}else if (condition == "SC")
 		{
-			/* code */
+			
 		}else if (condition == "SP")
 		{
-			/* code */
+			
 		}else if (condition == "SR")
 		{
-			/* code */
+			
 		}else if (condition == "ZZ")
-		{
-			/* code */
+		{	
 		}
-		cout << T << "*" << condition << "*" << s <<endl;
 	}
-	// bool *ptr=0;
-	// loc C = {1,1};
-	// Entity* A = new car("name",1,1,C,ptr,10,100,true,'N',10);
-	// A->print();
 	return 0;
 }
 
-
-car::car():Entity(){}
-car::car(string id,bool ison,bool isser,loc location,bool*attribute,int time,int score,bool high_level,char direction,int judge_time)
-:Entity(id,ison,isser,location,attribute,time),score(score),high_level(high_level),direction(direction),judge_time(judge_time)
+Passenger::Passenger():Entity(){}
+Passenger::Passenger(string id,bool*attribute,int time)
+:Entity(id,attribute,time)
 {}
+car::car():Entity(){}
+car::car(string id,bool*attribute,int time,bool high_level)
+:Entity(id,attribute,time),high_level(high_level)
+{
+	judge_time = 0;
+	direction = 'X';
+	score = 0;
+}
 Entity::Entity(){}
-Entity::Entity(string id,bool ison,bool isser,loc location,bool*attribute,int time)
-  : id(id), ison(ison), isser(isser), location(location), attribute(attribute),time(time){}
-
-
+Entity::Entity(string id,bool*attribute,int time)
+  : id(id),attribute(attribute),time(time){
+  	location = {0,0};
+  	ison = 0;
+  	isser = 0;
+  }
+void Passenger::print()
+{
+	Entity::print();
+}
 void Entity::print(){
 	// cout << "Entity:\n";
 	cout << "ID:" << id << endl;
 	cout << "ison" << ison << endl;
 	cout << "isser" << isser << endl;
 	cout << "location: "<< location.x << " " << location.y << endl; 
+	cout << "time: " << time << endl;
+	cout << "attribute: ";
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << attribute[i] << " ";
+	}cout << endl;
 }
 void car::print(){
 	cout << "Car\n";
@@ -130,4 +329,324 @@ void car::print(){
 	cout <<"high_level:" << high_level << endl;
 	cout <<"direction:" << direction << endl;
 	cout <<"judge_time:" << judge_time << endl;
+	
+}
+
+
+car NC_condition(string s,int time,string total_attribute[],int attributeN){
+	string _id = s.substr(0,s.find('('));
+	char level = s.substr(s.find(')')+1,string::npos)[0];
+	bool carlevel = 1;
+	if (level == 'R')
+	{
+		carlevel = false;
+	}
+	s = s.substr(s.find('(')+1,s.find(')'));
+	bool* tmpatt = new bool [attributeN];
+	for (int i = 0; i < attributeN; ++i)
+	{
+		tmpatt[i] = 0;
+	}
+    while(s.find(',') != string::npos){
+    	string stmp = s.substr(0,s.find(','));
+        s.erase(0,s.find(',')+1);
+        for (int i = 0; i < attributeN; ++i)
+        {
+        	if (stmp == total_attribute[i])
+        	{
+        		tmpatt[i] = 1;
+        		break;
+        	}
+        }
+    }
+    s = s.substr(0,s.find(')'));
+    for (int i = 0; i < attributeN; ++i)
+        {
+        	if (s == total_attribute[i])
+        	{
+        		tmpatt[i] = 1;
+        	}
+        }
+    car result(_id,tmpatt,time,carlevel);
+    return result;
+
+}
+
+
+
+Passenger NP_condition(string s,int time,string total_attribute[],int attributeN){
+	string _id = s.substr(0,s.find('('));
+	s = s.substr(s.find('(')+1,s.find(')'));
+	bool* tmpatt = new bool [attributeN];
+	for (int i = 0; i < attributeN; ++i)
+	{
+		tmpatt[i] = 0;
+	}
+    while(s.find(',') != string::npos){
+    	string stmp = s.substr(0,s.find(','));
+        s.erase(0,s.find(',')+1);
+        for (int i = 0; i < attributeN; ++i)
+        {
+        	if (stmp == total_attribute[i])
+        	{
+        		tmpatt[i] = 1;
+        		break;
+        	}
+        }
+    }
+    s = s.substr(0,s.find(')'));
+    for (int i = 0; i < attributeN; ++i)
+        {
+        	if (s == total_attribute[i])
+        	{
+        		tmpatt[i] = 1;
+        	}
+        }
+    Passenger result(_id,tmpatt,time);
+    return result;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------------
+//overload item operator=
+template <typename ItemType>
+Node<ItemType>::Node():next(nullptr){}
+template <typename ItemType>
+Node<ItemType>::Node(const ItemType& anItem):item(anItem),next(nullptr){
+	//overload assignment operator，constructor
+}
+template <typename ItemType>
+Node<ItemType>::Node(const ItemType& anItem, Node<ItemType>* nextNodePtr):next(nextNodePtr),item(anItem){}
+template<class ItemType>
+void Node<ItemType>::setItem(const ItemType& anItem)
+{
+  item = anItem;
+} 
+
+template<typename ItemType>
+void Node<ItemType>::setNext(Node<ItemType>* nextNodePtr)
+{
+  next = nextNodePtr;
+} 
+
+template<typename ItemType>
+ItemType Node<ItemType>::getItem() const
+{
+  return item;
+} 
+
+template<typename ItemType>
+Node<ItemType>* Node<ItemType>::getNext() const
+{
+  return next;
+} 
+
+template<typename ItemType>
+Node<ItemType>* Bag<ItemType>::getPointerTo(const ItemType& target) const{
+	bool found = false;
+	Node<ItemType>* tmptr = headPtr;
+	while(!found && (tmptr != nullptr)){
+		if (target == tmptr->getItem())
+		{
+			found = true;
+		}else{
+			tmptr = tmptr->getNext();
+		}
+	}
+	return tmptr;
+}
+
+template<typename ItemType>
+Bag<ItemType>::Bag():headPtr(nullptr),itemCount(0){}
+
+template<typename ItemType>
+Bag<ItemType>::Bag(const Bag<ItemType>& aBag){//copy--------
+	itemCount = aBag.itemCount;//??
+    Node<ItemType>* origChainPtr = aBag.headPtr;//??   
+    if(origChainPtr == nullptr)
+    	headPtr = nullptr;
+    else{
+    	headPtr = new Node<ItemType>();
+    	headPtr->setItem(origChainPtr->getItem());
+    	Node<ItemType>* newChainPtr = headPtr;
+    while(origChainPtr != nullptr)
+    {
+        origChainPtr = origChainPtr->getNext();
+        ItemType nextItem = origChainPtr->getItem();
+        Node<ItemType>* newNodePtr = new Node<ItemType>(nextItem);
+        newChainPtr->setNext(newNodePtr);
+        newChainPtr = newChainPtr->getNext();
+    }      
+    newChainPtr->setNext(nullptr);
+  } 
+}
+
+template<typename ItemType>
+int Bag<ItemType>::getCurrentSize() const{
+	return itemCount;
+}
+template<typename ItemType>
+bool Bag<ItemType>::isEmpty() const{
+	if (itemCount == 0)
+	{
+		return true;
+	}else{
+		return false;
+	}
+}
+template<typename ItemType>
+bool Bag<ItemType>::add(const ItemType& newEntry){//----copy
+	Node<ItemType>* newNodePtr = new Node<ItemType>();
+    newNodePtr->setItem(newEntry);
+    newNodePtr->setNext(headPtr);
+    headPtr = newNodePtr;
+    itemCount++; 
+    return true;
+}
+
+template<typename ItemType>
+bool Bag<ItemType>::remove(const ItemType& anEntry){
+	Node<ItemType>* entryNodePtr = getPointerTo(anEntry);
+  	bool canRemoveItem = !isEmpty() && (entryNodePtr != nullptr);
+  	if (canRemoveItem)
+  	{
+    	entryNodePtr->setItem(headPtr->getItem());
+    	Node<ItemType>* nodeToDeletePtr = headPtr;
+    	headPtr = headPtr->getNext();
+    	delete nodeToDeletePtr;
+    	nodeToDeletePtr = nullptr; 
+    	itemCount--;
+  	} 
+	return canRemoveItem;
+}
+
+template<typename ItemType>
+void Bag<ItemType>::clear(){//-----------copy
+	Node<ItemType>* nodeToDeletePtr = headPtr;
+  	while (headPtr != nullptr)
+    {
+    	headPtr = headPtr->getNext();
+    	nodeToDeletePtr->setNext(nullptr);
+    	delete nodeToDeletePtr;
+    	nodeToDeletePtr = headPtr;
+    }  
+    itemCount = 0;
+}
+
+template<typename ItemType>
+bool Bag<ItemType>::contains(const ItemType& anEntry) const{
+	if (getPointerTo(anEntry) == nullptr)
+	{
+		return false;
+	}else{
+		return true;
+	}
+}
+template<typename ItemType>
+Bag<ItemType>::~Bag(){
+	clear();
+}
+template <typename ItemType>
+Node<ItemType>* Bag<ItemType>::get(const ItemType& target){
+	Node<ItemType>* ptr;
+	ptr = getPointerTo(target);
+	return ptr;
+}
+
+template <typename ItemType>
+Node<ItemType>* Bag<ItemType>::get(string target){
+	bool found = false;
+	Node<ItemType>* tmptr = headPtr;
+	while(!found && (tmptr != nullptr)){
+		if (target == tmptr->getItem().getid())
+		{
+			found = true;
+		}else{
+			tmptr = tmptr->getNext();
+		}
+	}
+	return tmptr;
 }
--- bag_node.cpp
+++ bag_node.cpp
@@ -68,9 +68,33 @@ public:
   bool remove(const ItemType& anEntry);
   void clear();
   bool contains(const ItemType& anEntry) const;
+  ItemType get(const ItemType& target)const;
+  ItemType get(const string target)const;
   // int getFrequencyOf(const ItemType& anEntry) const;
   // vector<ItemType> toVector() const;
 };
+
+template <typename ItemType>
+ItemType Bag<ItemType>::get(const ItemType& target)const{
+	Node<ItemType>* ptr;
+	ptr = getPointerTo(target);
+	return ptr->getItem;
+}
+
+template <typename ItemType>
+ItemType Bag<ItemType>::get(const string target)const{
+	bool found = false;
+	Node<ItemType>* tmptr = headPtr;
+	while(!found && (tmptr != nullptr)){
+		if (target == tmptr->getItem().id)
+		{
+			found = true;
+		}else{
+			tmptr = tmptr->getNext();
+		}
+	}
+	return tmptr->getItem();
+}
 
 template<typename ItemType>
 Node<ItemType>* Bag<ItemType>::getPointerTo(const ItemType& target) const{
@@ -91,7 +115,26 @@ template<typename ItemType>
 Bag<ItemType>::Bag():headPtr(nullptr),itemCount(0){}
 
 template<typename ItemType>
-Bag<ItemType>::Bag(const Bag<ItemType>& aBag){}//---------------------copy constructor
+Bag<ItemType>::Bag(const Bag<ItemType>& aBag){//copy--------
+	itemCount = aBag.itemCount;
+    Node<ItemType>* origChainPtr = aBag.headPtr;   
+    if(origChainPtr == nullptr)
+    	headPtr = nullptr;
+    else{
+    	headPtr = new Node<ItemType>();
+    	headPtr->setItem(origChainPtr->getItem());
+    	Node<ItemType>* newChainPtr = headPtr;
+    while(origChainPtr != nullptr)
+    {
+        origChainPtr = origChainPtr->getNext();
+        ItemType nextItem = origChainPtr->getItem();
+        Node<ItemType>* newNodePtr = new Node<ItemType>(nextItem);
+        newChainPtr->setNext(newNodePtr);
+        newChainPtr = newChainPtr->getNext();
+    }      
+    newChainPtr->setNext(nullptr);
+  } 
+}
 
 template<typename ItemType>
 int Bag<ItemType>::getCurrentSize() const{
@@ -133,7 +176,16 @@ bool Bag<ItemType>::remove(const ItemType& anEntry){
 }
 
 template<typename ItemType>
-void Bag<ItemType>::clear(){//---------------------------
+void Bag<ItemType>::clear(){//-----------copy
+	Node<ItemType>* nodeToDeletePtr = headPtr;
+  	while (headPtr != nullptr)
+    {
+    	headPtr = headPtr->getNext();
+    	nodeToDeletePtr->setNext(nullptr);
+    	delete nodeToDeletePtr;
+    	nodeToDeletePtr = headPtr;
+    }  
+    itemCount = 0;
 }
 
 template<typename ItemType>
@@ -145,8 +197,20 @@ bool Bag<ItemType>::contains(const ItemType& anEntry) const{
 		return true;
 	}
 }
+template<typename ItemType>
+Bag<ItemType>::~Bag(){
+	clear();
+}
 
+
+// template<typename ItemType>
+void fii(Bag<int> u){}
 int main(int argc, char const *argv[])
 {
+	Bag<int> oo;
+	int k = 1;
+	oo.add(k);
+	fii(oo);
+
 	return 0;
 }
