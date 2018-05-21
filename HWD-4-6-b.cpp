25f8252db1f24247b5450bb86b3d02775277f638
Taxod <s885537@gmail.com>
2018-05-21 10:55:11 +0800

    -

--- HWD-4-6-b.cpp
+++ HWD-4-6-b.cpp
@@ -8,7 +8,20 @@ int main(int argc, char const *argv[])
 }
 
 template <typename ItemType>
+class stack : public stackInterface<ItemType>
+{
+private:
+	ItemType *item;
+	int top;
+public:
+	ArrayStack();
+	bool isEmpty() const;
+	bool push(const ItemType& newEntry);
+	bool pop();
+	ItemType peek() const;
+}
 
+template <typename ItemType>
 bool ArrayStack<ItemType>::push(const ItemType& newEntry){
 	bool result = false;
 	if (top < MAX_STACK-1)
@@ -17,7 +30,26 @@ bool ArrayStack<ItemType>::push(const ItemType& newEntry){
 		item[top] = newEntry;
 		result = true;
 	}else{
-		
+		tmp = new ItemType [MAX_STACK];
+		for (int i = 0; i < MAX_STACK; ++i)
+		{
+			tmp[i] = item[i];
+		}
+		delete [] item;
+		oringnal_len = MAX_STACK;
+		MAX_STACK += MAX_STACK;
+		item = new ItemType [MAX_STACK];
+		for (int i = 0; i < oringnal_len; ++i)
+		{
+			item[i] = tmp[i];
+		}
+		top++;
+		item[top] = newEntry;
+		result = true;
 	}
 	return result;
+}
+template <typename ItemType>
+ArrayStack<ItemType>::ArrayStack():top(-1){
+	item = new ItemType [MAX_STACK];
 }
