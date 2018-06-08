c70c9b006e33725a29f4c259b949962abcd042ff
Taxod <s885537@gmail.com>
2018-06-08 22:08:12 +0800

    -

---
+++ HWD-5-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
+
+while(輸入字元){
+	if(輸入的字元!= <-){
+		deque.add_front(字元);
+	}else{
+		deque.remove_front();
+	}	
+}
